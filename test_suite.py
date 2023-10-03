8615e6b953e90ed43730c9bdb155c5638b2b3b4c
Brice <bricebro@usc.edu>
2023-10-03 15:28:46 -0700

    finished initial test suite

--- test_suite.py
+++ test_suite.py
@@ -18,6 +18,43 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 # get reading from adc 
 # mcp.read_adc(adc_channel)
 
+def blink_led(blinks, interval):
+    toggle = False
+    for _ in range(blinks * 2):  # Multiply by 2 because one ON and one OFF constitutes a single blink
+        if toggle:
+            GPIO.output(chan_list, GPIO.LOW)
+        else:
+            GPIO.output(chan_list, GPIO.HIGH)
+        toggle = not toggle
+        time.sleep(interval)
+
 while True: 
-  time.sleep(0.5) 
+  # Test 1, flicker 5 times with a 500ms interval
+  blink_led(5, .5)
+
+  # Test 2, for 5 seconds read light sensor
+  LIGHT_THRESHOLD = 500
+  for i in range(1,51):
+    lightValue = mcp.read_adc(0)
+    if lightValue < LIGHT_THRESHOLD:
+      print(lightValue + " dark")
+    else:
+       print(lightValue + " bright")
+    time.sleep(0.1)
+
+  # Test 3, flicker 4 times with a 200ms interval
+  blink_led(4, .2)
+
+  # Test 4, for 5 seconds read sound sensor
+  SOUND_THRESHOLD = 800
+  tapped = False
+  for i in range(1,51):
+    soundValue = mcp.read_adc(0)
+    print(soundValue)
+    if soundValue > SOUND_THRESHOLD:
+       GPIO.output(chan_list, GPIO.HIGH)
+       tapped = True
+    time.sleep(0.1)
+    if(tapped):
+      GPIO.output(chan_list, GPIO.LOW)
  
